16ee11ba6b219dc3e3ef3519655eaaeddce9702f
Jean-Daniel Bancal <jdbancal.physics@gmail.com>
2021-12-13 20:33:10 +0100

    supporting more link formats

--- replab_quantum_generate.m
+++ replab_quantum_generate.m
@@ -53,10 +53,9 @@ function result = replab_quantum_generate(what)
         replab.infra.cleanDir(fullfile(pathStr, 'docs'), {'.git'});
         if ~isequal(what, 'clear')
             % Create a modifiable copy of the sphinx folder
-            copyfile(fullfile(pathStr, 'sphinx/*'), fullfile(pathStr, '_sphinx'))
+            copyfile(fullfile(pathStr, 'sphinx/*'), fullfile(pathStr, '_sphinx'));
 
-            % Modify API links in all matlab file. First, we load the table
-            % of available links
+            % Load the conversion table to create API links in matlab files
             baseWeb = 'https://replab.github.io/replab';
             if unix(['python -m sphinx.ext.intersphinx https://replab.github.io/replab/objects.inv > ', pathStr, '/_sphinx/API_links.txt'])
                 warning('API conversion table not found, cross-links will not work in .m files');
@@ -68,19 +67,28 @@ function result = replab_quantum_generate(what)
             fileList = regexp(fileList, '\n', 'split');
             fileList = fileList(1:end-1);
             if status == 0
+                pb = replab.infra.repl.ProgressBar(length(fileList));
                 for i = 1:length(fileList)
+                    pb.step(i, fileList{i});
                     content = replab.infra.CodeTokens.fromFile(fileList{i});
                     lines = content.lines;
                     for j = find(content.tags == '%')
                         line = lines{j};
-                        extents = regexp(line, '(`\+replab\.[\w,\.]+`)', 'tokenExtents');
+                        extents = regexp(line, '(`~?\+replab\.[\w,\.]+`|`~?root\.[\w,\.]+`)', 'tokenExtents');
                         extents{end+1} = length(line)+1;
                         if ~isempty(extents)
                             newLine = line(1:extents{1}(1)-1);
                             for k = 1:length(extents)-1
                                 token = line(extents{k}(1)+1:extents{k}(2)-1);
-                                onlyName = regexp(token, '\.*(\w+)$', 'tokens');
-                                onlyName = onlyName{1}{1};
+                                silent = (token(1) == '~');
+                                if silent
+                                    tokenName = regexp(token, '\.*(\w+)$', 'tokens');
+                                    tokenName = tokenName{1}{1};
+                                    token = token(2:end);
+                                else
+                                    tokenName = token;
+                                end
+                                tokenName = tokenName(tokenName ~= '+');
                                 [status, match] = unix(['cat ', pathStr, '/_sphinx/API_links.txt | grep "^[[:space:]]*', token, '\ "']);
                                 if status == 0
                                     if length(regexp(match(1:end-1), '\n', 'split')) > 1
@@ -88,7 +96,7 @@ function result = replab_quantum_generate(what)
                                     end
                                     link = regexp(match(1:end-1), '\ ([^\ ]+)$', 'tokens');
                                     link = [baseWeb, '/', link{1}{1}];
-                                    newLine = [newLine, '[', onlyName, '](', link,')'];
+                                    newLine = [newLine, '[', tokenName, '](', link,')'];
                                 else
                                     warning(['Reference ', token, ' was not found in the API.']);
                                     newLine = [newLine, token];
@@ -104,9 +112,9 @@ function result = replab_quantum_generate(what)
                     end
                     fclose(fid);
                 end
+                pb.finish;
             end
 
-            
             % Now launch sphinx in modified folder
             disp('Running Sphinx');
             lastPath = pwd;
